3b6ad4ea728021d054e849df36f2048a0d433fbc
alexismtz98 <alexiaums98@gmail.com>
2021-10-16 09:14:53 -0500

    REV-17
    Se removieron componentes no utilizados para prueba de despliegue.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Homepage from './components/homepage/homepage';
-import Login from './components/login/login';
+
 import Register from './components/register/register';
 
 function App() {
     return ( 
         <div className = "App" >
-        {/* <Homepage /> */}
-        {/*  <Login /> */}
+
         <Register /> 
         
         </div>
